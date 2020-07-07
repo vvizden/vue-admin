125e26dcacb49e76df44841dc13bb80afc84c59a
vviz <820853182@qq.com>
2020-07-07 13:48:37 +0800

    左侧栏过渡优化

--- src/layouts/components/Aside.vue
+++ src/layouts/components/Aside.vue
@@ -1,5 +1,11 @@
 <template>
-  <el-aside class="sidebar-container" :width="variables.sideBarWidth">
+  <el-aside
+    class="sidebar-container"
+    :width="variables.sideBarWidth"
+    :class="sidebarClass"
+    :style="sidebarStyle"
+    @transitionend.native.self="handleTransitionend"
+  >
     <div
       class="menu-collapse"
       :style="{ color: variables.primaryColor }"
@@ -17,6 +23,12 @@
 import { mapGetters } from 'vuex'
 import variables from '@/styles/variables.scss'
 
+const calcWidthScale = (min, max) => {
+  const minNum = parseInt(min, 10)
+  const maxNum = parseInt(max, 10)
+  return [minNum / maxNum, maxNum / minNum]
+}
+
 export default {
   name: 'Aside',
   components: {
@@ -24,8 +36,15 @@ export default {
     Item: () => import(/* webpackChunkName: "layout" */ './RouterMenu/Item'),
   },
   data() {
+    this.computedWidthScale = calcWidthScale(
+      variables.sideBarCollapseWidth,
+      variables.sideBarWidth,
+    )
     return {
       variables,
+      menuCollapse: null,
+      sidebarStyle: {},
+      sidebarClass: {},
     }
   },
   computed: {
@@ -37,9 +56,42 @@ export default {
       return this.isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'
     },
   },
+  watch: {
+    isCollapse(val) {
+      this.sidebarClass = {
+        'sidebar-flip-animation': true,
+      }
+
+      this.sidebarStyle = {
+        transform: val
+          ? `scale(${this.computedWidthScale[1]}, 1)`
+          : `scale(${this.computedWidthScale[0]}, 1)`,
+      }
+
+      this.sidebarInnerStyle = {
+        transform: `scale(1, 1)`,
+      }
+
+      requestAnimationFrame(() => {
+        this.sidebarStyle = {
+          ...this.sidebarStyle,
+          transition: 'transform 0.2s',
+        }
+
+        this.sidebarStyle = {
+          ...this.sidebarStyle,
+          transform: '',
+        }
+      })
+    },
+  },
   methods: {
     toggleCollapse() {
       this.$store.dispatch('app/toggleSideBarCollpase')
+    },
+    handleTransitionend() {
+      this.sidebarClass = {}
+      this.sidebarStyle = {}
     },
   },
 }
