dac9cd3391a5c2067d292d2796c191426c8333ff
vviz <820853182@qq.com>
2020-07-16 17:39:57 +0800

    修改表单混入

--- src/mixins/form-mixin.js
+++ src/mixins/form-mixin.js
@@ -2,12 +2,14 @@ import { cloneDeep } from 'lodash-es'
 
 /* !!!!!! 使用时需要提供 model, ruleFormReset, ruleForm 数据和 manualReset, formToFormData 方法 */
 export default {
-  methods: {
+  computed: {
     isEditForm() {
       return (
         this.model && Object.prototype.hasOwnProperty.call(this.model, 'id')
       )
     },
+  },
+  methods: {
     // 重置表单
     resetForm(formName) {
       if (this.$refs[formName]) {
@@ -28,7 +30,7 @@ export default {
           if (this.localHttp) {
             httpPromise = this.localHttp()
           } else {
-            if (this.isEditForm()) {
+            if (this.isEditForm) {
               httpPromise = this.$http.put(this.url.edit, formData)
             } else {
               httpPromise = this.$http.post(this.url.create, formData)
