3500f7d9053565154f53520dff8ed34bfaa09245
vviz <820853182@qq.com>
2020-08-26 11:23:23 +0800

    权限函数及指令

--- src/directives/permission/permission.js
+++ src/directives/permission/permission.js
@@ -1,31 +1,18 @@
-import store from '@/store'
+import { checkPermission } from '@/utils/permission'
 
-function checkPermission(el, binding) {
+function check(el, binding) {
   const { value } = binding
-  const userPermissions = store.getters.userPermissions
-
-  if (value && value instanceof Array) {
-    if (value.length > 0) {
-      const hasPermission = userPermissions.some((permission) => {
-        return value.includes(permission)
-      })
-
-      if (!hasPermission) {
-        el.parentNode && el.parentNode.removeChild(el)
-      }
-    }
-  } else {
-    throw new Error(
-      `need permissions! Like v-permission="['user:add','user:edit']"`,
-    )
+  const hasPermission = checkPermission(value)
+  if (!hasPermission) {
+    el.parentNode && el.parentNode.removeChild(el)
   }
 }
 
 export default {
   inserted(el, binding) {
-    checkPermission(el, binding)
+    check(el, binding)
   },
   update(el, binding) {
-    checkPermission(el, binding)
+    check(el, binding)
   },
 }
